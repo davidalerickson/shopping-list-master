c2513d7dca63a92024f2bbab9f13ec4b09fa7bef
davidalerickson <davidalerickson@hotmail.com>
2017-02-16 16:12:19 -0500

    completed again - fixed updating of checked value

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 //single state object
 var state = {
   items: [],
-
 };
 //state modification functions
 var addItem = function(state, item){
-  state.items.push(item);
-  //alert(state.items);
+  state.items.push({itemName: item, checkState: "checked"});
+  //alert(state.items[0].checkState);
 };
 
 var deleteItem = function(state, item){
   delete state.items[item];
-  //alert(state.items);
 };
 
 //render functions
 var renderList = function(state, element){
   var itemsHTML =  state.items.map(function(item, index){
+    var ItemClasses = "";
+    if(item.checkState === 1){
+      ItemClasses = "shopping-item shopping-item__checked";
+    }else{
+      ItemClasses = "shopping-item";
+    }
     varItemHTMLString = `
     <li>
-      <span class="shopping-item" data-list-index="${index}">${item}</span>
+      <span class="${ItemClasses}" data-list-index="${index}">${item.itemName}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle">
           <span class="button-label">check</span>
@@ -37,22 +41,23 @@ var renderList = function(state, element){
 
 
 $(function(){
-  //Initialize State;
 
   //Event Listeners
+  //Add Item Button
   $("#js-shopping-list-form").submit(function(event){
     event.preventDefault();
     //alert("was clicked");
     addListItem();
   })
-
+  //Check-Uncheck List Item
   function addCheckEventToggle(){
     $(".shopping-item-toggle").on('click', function(event){
-      //alert("Clicked the check button");
+      var itemToToggle = $(this).parents().siblings(".shopping-item").attr("data-list-index");
       $(this).parents().siblings(".shopping-item").toggleClass("shopping-item__checked");
+      updateListItem(itemToToggle);
     });
   }
-
+  ///Delete Button
   function addDeleteEvents(){
     $(".shopping-item-delete").on('click', function(event){
       var listItemToDelete =
@@ -64,6 +69,14 @@ $(function(){
   function addListItem(){
     addItem(state, $("#shopping-list-entry").val());
     updateHTMLFromState();
+  }
+
+  function updateListItem(itemToToggle){
+    if(state.items[itemToToggle].checkState === 1){
+      state.items[itemToToggle].checkState = 0;
+    }else{
+      state.items[itemToToggle].checkState = 1;
+    }
   }
 
   function deleteListItem(listItemToDelete){
